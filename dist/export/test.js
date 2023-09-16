8f304876fe4787be369daf400e2c07a6309d6fa7
Mohammad Betamony <mbayta@outlook.com>
2023-09-16 17:59:03 +0300

    Init project

---
+++ dist/export/test.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greet = void 0;
+const greet = () => { console.log('Hello baytamonie'); };
+exports.greet = greet;
+//# sourceMappingURL=test.js.map
---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const test_1 = require("./export/test");
+(0, test_1.greet)();
+//# sourceMappingURL=index.js.map
